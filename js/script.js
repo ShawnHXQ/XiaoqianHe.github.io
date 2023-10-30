eafbd42d7a0f21a88a14b4b405917dd42c8b4d16
ShawH <sevenhxq@gmail.com>
2023-10-30 12:00:24 -0400

    change the role info

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* ===============typing animation ================*/
 var typed = new Typed(".typing",{
-    strings:["","Student", "Web Designer", "Software Developer", "..."], 
+    strings:["","Full-Stack Developer", "Web Designer", "Software Developer"], 
     typeSpeed:100, 
     BackSpeed:60, 
     loop:true
